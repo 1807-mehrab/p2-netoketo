d078cb5c16a93f0dd4ed4cd50ff7f5c3c533e1aa
kyleabutterfield <kyleabutterfield@Yahoo.com>
2018-08-17 17:56:28 -0500

    Updated SQL file edited tables

--- SQLFiles/Creates.sql
+++ SQLFiles/Creates.sql
@@ -1,27 +1,30 @@
 Drop Table Images;
 Drop Table RecipeRating;
 Drop Table Comments;
-Drop Table Recpies;
+Drop Table Recipes;
 Drop Table Accounts;
 
 
 CREATE TABLE Accounts(
 	AccountID int Primary Key,
-	Username VarChar2(50),
-	Password VarChar2(50),
-	Email VARCHAR2(50),
+	Username VarChar2(50) Unique Not Null,
+	Password VarChar2(50) Not Null,
+	Email VARCHAR2(50) Not Null,
 	AccountType int
 );
 
 
 CREATE TABLE Recipes(
 	RecipeID int Primary Key,
-	RecipeName varchar2(50),
+	RecipeName varchar2(50) Not Null,
 	Deleted int,
 	DeletedBy int,
-	OwnerID int,
+	OwnerID int Not Null,
 	Flagged int,
-	DateCreated date,
+	DateCreated TIMESTAMP,
+	Description varchar2(300) Not NULL,
+	Ingredients varchar2(300) Not NULL,
+	CookingInstructions VARCHAR2(300) Not NULL,
 	Foreign Key (DeletedBy) REFERENCES Accounts (AccountID) on delete cascade,
 	Foreign Key (OwnerID) REFERENCES Accounts (AccountID) on delete cascade
 );
@@ -29,20 +32,21 @@ CREATE TABLE Recipes(
 
 CREATE TABLE Comments(
 	CommentID int Primary Key,
-	Content VarChar2(300),
+	Content VarChar2(300) Not NULL,
 	Flagged int,
-	RecipeID int,
-	OwnerID int,
+	RecipeID int Not NULL,
+	OwnerID int Not NULL,
+	DateCreated TIMESTAMP,
 	FOREIGN KEY (RecipeID) REFERENCES Recipes (RecipeID) on delete cascade,
 	FOREIGN KEY (OwnerID) REFERENCES Accounts (AccountID) on delete cascade
 );
 
 CREATE TABLE RecipeRating(
 	RatingID int PRIMARY Key,
-	OwnerID int,
-	RecipeID int,
+	OwnerID int Not NULL,
+	RecipeID int Not NULL,
 	ValNum int,
-	DateCreated date,
+	DateCreated TIMESTAMP,
 	FOREIGN Key (RecipeID) References Recipes (RecipeID) on delete cascade,
 	Foreign Key (OwnerID) References Accounts (AccountID) on delete cascade
 );
