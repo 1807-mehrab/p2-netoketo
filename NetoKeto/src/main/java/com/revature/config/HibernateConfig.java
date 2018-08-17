116907b64fcf226e3b3309314b9875c9c9ce0d51
timtouch <chaosbounder@gmail.com>
2018-08-16 19:44:21 -0500

    Add an endpoint to return page of users

--- NetoKeto/src/main/java/com/revature/config/HibernateConfig.java
+++ NetoKeto/src/main/java/com/revature/config/HibernateConfig.java
@@ -25,7 +25,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-import com.revature.repository.UserDao;
 
 
 @Configuration
--- NetoKeto/src/main/java/com/revature/controller/UserController.java
+++ NetoKeto/src/main/java/com/revature/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.revature.controller;
 
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/profile")
 public class UserController {
+	@Autowired
+	private UserService us;
+
 	@RequestMapping(value="/username", method=RequestMethod.GET)
 	public ResponseEntity<String> usernameGet() {
 		String username = "username here";
 		return ResponseEntity.ok(username);
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public ResponseEntity<String> getAllUsers(){
+		String users = us.getAllUsers().toString();
+		return ResponseEntity.ok(users);
 	}
 }
