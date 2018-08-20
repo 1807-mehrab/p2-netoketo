0728177396dd3651255373f472a0a9f61e8c3b25
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-08-20 17:20:44 -0500

    Login POST Functionality Established

--- NetoKeto/src/main/java/com/revature/controller/LoginController.java
+++ NetoKeto/src/main/java/com/revature/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.revature.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.beans.User;
 import com.revature.services.LoginService;
 
@@ -20,14 +23,40 @@ public class LoginController {
 	@Autowired
 	private LoginService ls;
 	
+	/*
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ResponseEntity<User> authenticator(@RequestBody User user) {
-		System.out.print("Requested");
+		System.out.print("Requested: ");
 		System.out.print(user);
 		User u = ls.authenticate(user);
 		return ResponseEntity.status(HttpStatus.OK)
 				//.header("status","200")
 				.body(u);
+	}
+	*/
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<User> authenticator(@RequestBody String inpt) {
+		System.out.print("Requested: ");
+		System.out.println(inpt);
+		inpt = inpt.replaceFirst("\"login\":", "");
+		inpt = inpt.substring(1);
+		inpt = inpt.substring(0, (inpt.length()-1));
+		System.out.println("New: ");
+		System.out.println(inpt);
+		ObjectMapper mapper = new ObjectMapper();
+		User auth = new User();
+		try {
+			auth = mapper.readValue(inpt, User.class);
+			System.out.println("Final: " + auth);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.print("CONVERSION FAILURE");
+		}
+		
+		User u = ls.authenticate(auth);
+		System.out.print("Returning: ");
+		System.out.println(u);
+		return ResponseEntity.ok(u);
 	}
 	
 	@GetMapping(value="/login")
--- ember-front-end/app/adapters/user.js
+++ ember-front-end/app/adapters/user.js
@@ -2,6 +2,6 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
     pathForType() {
-        return 'users/Admin'
+        return 'users/joberholtzer'
     }
 });
