c7a21b6849781f4a0dd8a524fc5ab32a9dadd4b7
John Oberholtzer <johnpauloberholtzer@gmail.com>
2018-08-22 13:55:46 -0500

    Fixed functionality of Creating New Recipes

--- ember-front-end/app/routes/new-recipe.js
+++ ember-front-end/app/routes/new-recipe.js
@@ -1,44 +1,52 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
     model() {
-        user :this.store.peekAll('login');
-        recipe : this.store.findAll('recipe')
+        return RSVP.hash({
+            user :this.store.peekAll('login'),
+            recipe : this.store.findAll('recipe')
+        });
     },
     actions: {
         newRecipe(rname,rdescription,ringredients, ckinstructions) {
-            let currUser = this.store.peekAll('login').then(function(logins){
-                return logins.get("firstObject");
-            })
-            var onSuccess = function(){
-                console.log("success");
-                self.refresh;
-                self.transitionTo('login');
-            }
-            var onFail = function(){
-                console.log("failure");
-                console.log(rname);
-                console.log(rdescription);
-                console.log(ringredients);
-                console.log(ckinstructions);
-                self.refresh;
-            }
+            let logindata = this.store.peekAll('login');
+            let currUser = logindata.get("firstObject");
             
+            let usname = currUser.get('username')
+            console.log(usname);
+
             var self = this;
-            this.store.createRecord('recipe', {
-                recipeId: '1', 
-                recipeName: rgusername, 
-                deleted: null,
-                deletedBy: null,
-                owner: currUser,
-                flagged: null,
-                dateCreated: new Date(),
-                description: rdescription,
-                ingredients: ringredients,
-                cookingInstructions: ckinstructions,
-                image: null 
-            })
-            .save().then(onSuccess,onFail);
+            this.store.findRecord('user',usname).then(function(RL){
+                console.log('User Is: '+RL.get('username'));
+                var onSuccess = function(){
+                    console.log("success");
+                    self.refresh;
+                    self.transitionTo('recipe');
+                }
+                var onFail = function(){
+                    console.log("failure");
+                    console.log(rname);
+                    console.log(rdescription);
+                    console.log(ringredients);
+                    console.log(ckinstructions);
+                    self.refresh;
+                }
+                self.store.createRecord('recipe', {
+                    recipeId: '1', 
+                    recipeName: rname, 
+                    deleted: false,
+                    deletedBy: null,
+                    owner: RL,
+                    flagged: false,
+                    dateCreated: new Date(),
+                    description: rdescription,
+                    ingredients: ringredients,
+                    cookingInstructions: ckinstructions,
+                    image: null 
+                })
+                .save().then(onSuccess,onFail);
+            });    
  
         }
     }
