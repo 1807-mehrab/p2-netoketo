499a9770e550fc4adcd0888463c4d428eaca6e6c
calahown <blind.noldor@gmail.com>
2018-08-22 10:54:03 -0500

    Recipe Page

--- ember-front-end/app/models/recipe.js
+++ ember-front-end/app/models/recipe.js
@@ -4,12 +4,12 @@ export default DS.Model.extend({
   recipeId: DS.attr('number'),
   recipeName: DS.attr('string'),
   deleted: DS.attr('number'),
-  deletedBy: DS.belongsTo('user'),
-  owner: DS.belongsTo('user'),
+  deletedBy: DS.attr(),
+  owner: DS.attr(),
   flagged: DS.attr('number'),
   dateCreated: DS.attr('date'),
   description: DS.attr('string'),
   ingredients: DS.attr('string'),
   cookingInstructions: DS.attr('string'),
-  image: DS.hasMany('image')
+  image: DS.attr()
 });
