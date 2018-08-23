aa49385a31deffd9fde6b7c2baae2ea89cb0034f
calahown <blind.noldor@gmail.com>
2018-08-23 09:25:54 -0500

    Upvote Downvote v1

--- ember-front-end/app/models/rating.js
+++ ember-front-end/app/models/rating.js
@@ -2,8 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   ratingId: DS.attr('number'),
-  owner: DS.belongsTo('user'),
-  recipe: DS.belongsTo('recipe'),
+  owner: DS.attr(),
+  recipe: DS.attr(),
   valNum: DS.attr('number'),
   dateCreated: DS.attr('date')
 });
