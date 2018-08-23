e931a8fa8ac804f1fa15f432c15a1690092de967
calahown <blind.noldor@gmail.com>
2018-08-22 20:50:08 -0500

    Post a Comment

--- ember-front-end/app/routes/show.js
+++ ember-front-end/app/routes/show.js
@@ -5,17 +5,27 @@ export default Route.extend({
   model(params) {
     return RSVP.hash({
       recipe: this.store.findRecord('recipe', params.recipeid),
-      comments: this.store.findAll('comment')
+      comments: this.store.findAll('comment', params.recipeid)
 
     });
   },
-  checkid(crid, rid) {
-    if (crid == rid) {
-      return true;
-    }
-    else {
-      return false;
-    }
+  actions: {
+      createComment(comment, recipeitem) {
+          let logindata = this.store.peekAll('login');
+          let currUser = logindata.get("firstObject");
+          let usname = currUser.get('username');
+          let self = this;
+          this.store.findRecord('user',usname).then(function(RL){
+            self.store.createRecord('comment', {
+              commentId: 1,
+              content: comment,
+              flagged: 0,
+              recipe: recipeitem,
+              owner: RL,
+              createdOn: new Date()
+            }).save().then(function() { self.refresh;})
+          })
+      }
   }
 
 });
